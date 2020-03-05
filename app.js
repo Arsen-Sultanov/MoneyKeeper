eafb9fe3f81c88fc050c98e14f63d21b8f1e6db0
Arsen-Sultanov <sultanov.a.i13@gmail.com>
2020-03-05 21:56:32 +0300

    init

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('./sw.js', {scope: './'})
       .then(reg=> {
         if(reg.installing) {
             console.log('sw installing');

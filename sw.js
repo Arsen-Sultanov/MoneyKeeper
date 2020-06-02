44d68dff4aa11b41f1bf9f3062a6dabaaf32530a
Arsen-Sultanov <sultanov.a.i13@gmail.com>
2020-06-02 13:19:52 +0300

    wip

--- sw.js
+++ sw.js
@@ -3,11 +3,16 @@ this.addEventListener('install', function(event) {
         caches.open('v1')
         .then(function(cache) {
               return cache.addAll([
-                '/',
-                '/MoneyKeeper/index.html',
-                '/MoneyKeeper/app.js'
+                './index.html',
+                './app.js'
               ])
         })
         .catch(error=> console.log('sw error: ' + error))
     );
+});
+this.addEventListener('fetch', (event) => {
+  console.log(111);
+  event.respondWith(
+    caches.match(event.request)
+  );
 });
